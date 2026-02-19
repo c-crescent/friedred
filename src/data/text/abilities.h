f284e676101793424d9553423eb61ab74f970260
CynthiaCrescent <lightredemption@hotmail.com>
2026-02-19 22:21:35 +0700

    Implemented Slush Rush and distributed it

--- src/data/text/abilities.h
+++ src/data/text/abilities.h
@@ -79,6 +79,7 @@ static const u8 sAirLockDescription[] = _("Negates weather effects.");
 static const u8 sSlowStartDescription[] = _("Temporarily halves speed and attack.");
 static const u8 sTechnicianDescription[] = _("Powers up the POKéMON's weaker moves.");
 static const u8 sNoGuardDescription[] = _("Prevents moves from missing.");
+static const u8 sSlushRushDescription[] = _("Raises SPEED in hail.");
 
 const u8 *const gAbilityDescriptionPointers[ABILITIES_COUNT] =
 {
@@ -163,6 +164,7 @@ const u8 *const gAbilityDescriptionPointers[ABILITIES_COUNT] =
     [ABILITY_SLOW_START] = sSlowStartDescription,
     [ABILITY_TECHNICIAN] = sTechnicianDescription,
     [ABILITY_NO_GUARD] = sNoGuardDescription,
+    [ABILITY_SLUSH_RUSH] = sSlushRushDescription,
 };
 
 const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
@@ -248,4 +250,5 @@ const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
     [ABILITY_SLOW_START] = _("SLOW START"),
     [ABILITY_TECHNICIAN] = _("TECHNICIAN"),
     [ABILITY_NO_GUARD] = _("NO GUARD"),
+    [ABILITY_SLUSH_RUSH] = _("SLUSH RUSH"),
 };
